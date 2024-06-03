b5cb2b83d8649b89aab6950d2469b192619ed4b1
Sophie-Ahn <sgdream95@gmail.com>
2024-06-03 17:59:41 +0900

    상속

---
+++ src/extends1/ex1/ElectricCar.java
@@ -0,0 +1,11 @@
+package extends1.ex1;
+
+public class ElectricCar {
+    public void move(){
+        System.out.println("치가 움직인다.");
+    }
+
+    public void charge(){
+        System.out.println("차가 충전하고 있어요.");
+    }
+}
---
+++ src/extends1/ex1/GasCar.java
@@ -0,0 +1,11 @@
+package extends1.ex1;
+
+public class GasCar {
+    public void move(){
+        System.out.println("차가 움직입니다.");
+    }
+
+    public void fillUp(){
+        System.out.println("차가 기름을 먹고 있습니다.");
+    }
+}
